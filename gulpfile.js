5d5028f0e5c51a0ac0f60b502c0a71ceab1e3e4d
BreathlessWay <731005087@qq.com>
2021-04-13 13:37:16 +0800

    style: update

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const { src, dest, watch, parallel, lastRun } = require("gulp"),
   gulpIf = require("gulp-if"),
   pump = require("pump"),
   imagemin = require("gulp-imagemin"),
+  pngquant = require("imagemin-pngquant"),
   cache = require("gulp-cache"),
   prettyData = require("gulp-pretty-data"),
   tap = require("gulp-tap"),
@@ -73,7 +74,18 @@ const copyHelpers = () => {
 
 const copyImages = () => {
   return src(fileInputPath.images, { since: since(copyImages) })
-    .pipe(cache(imagemin()))
+    .pipe(
+      cache(
+        imagemin({
+          optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+          progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+          interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+          multipass: true, //类型：Boolean 默认：false 多次优化svg直到完全优化
+          svgoPlugins: [{ removeViewBox: false }], //不要移除svg的viewbox属性
+          use: [pngquant()], //使用pngquant深度压缩png图片的imagemin插件
+        })
+      )
+    )
     .pipe(dest(`${outputPath}/images`));
 };
 
